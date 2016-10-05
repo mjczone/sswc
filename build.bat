c8cc760ab608be8566583468f96c6b0523396eec
mattjcowan <mjc@mjczone.com>
2016-10-05 16:22:00 -0500

    updated zip file

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ set dist=%spath%\dist
 mkdir "%dist%"
 del "%dist%\sswc.zip"
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" src\sswc\sswc.csproj /p:Configuration=Release /p:OutputPath="%dist%"
-lib\7za.exe a dist\sswc.zip "%dist%\*"
+lib\7z.exe a dist\sswc.zip "%dist%\*"
 cd "%dist%"
 for /f %%F in ('dir /b /a-d ^| findstr /vile ".zip .md .bat"') do del "%%F"
 cd ..
